7a13951c0a445e09568b42e84b4f8561efebe464
Adrian <>
2018-10-25 14:47:46 -0600

    fixes to 'logout' link

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jbuilder', '~> 2.5'
 
  gem 'bootstrap-sass'
  gem 'bootstrap','~>4.0.0.alpha3'
+ gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :courses
 
 root 'home_page#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
